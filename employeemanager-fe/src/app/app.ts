0e21a16daadc59436ffe930b6293ffc1e650aebc
Mai Dang <danglemaikhanh@gmail.com>
2026-01-05 18:16:13 +0100

    added employee component

--- employeemanager-fe/src/app/app.ts
+++ employeemanager-fe/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Employee } from './employee/employee';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Employee],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
