57e09828d0d1f519219d0ad9c01e231a915d751e
Mai Le <s86123@bht-berlin.de>
2026-01-05 14:36:37 +0100

    backend init

---
+++ employeemanager-be/src/main/java/com/dlmk/employeemanager_be/EmployeemanagerBeApplication.java
@@ -0,0 +1,13 @@
+package com.dlmk.employeemanager_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerBeApplication.class, args);
+	}
+
+}
